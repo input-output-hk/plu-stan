21dc69e14d0e8bf0ab3b89448b5fbae68a4b966a
Bogdan Manole <bogdan.manole@frago.ro>
2024-07-23 15:21:51 +0300

    tmp: remove plutustx

--- target/Target/PlutusTx.hs
+++ target/Target/PlutusTx.hs
@@ -9,14 +9,14 @@ import qualified Data.HashSet as HS
 import qualified Data.List as List
 import qualified Data.Text as Text
 
-import qualified PlutusTx as Tx
-import qualified PlutusTx.Maybe as Maybe
-import qualified PlutusTx.Prelude as Tx
-import qualified PlutusTx.AssocMap as AssocMap
-import PlutusTx (UnsafeFromData(unsafeFromBuiltinData))
-
--- Place for future imports
-import PlutusLedgerApi.V1 (PubKeyHash(..),Credential(..),ScriptHash(..))
+-- import qualified PlutusTx as Tx
+-- import qualified PlutusTx.Maybe as Maybe
+-- import qualified PlutusTx.Prelude as Tx
+-- import qualified PlutusTx.AssocMap as AssocMap
+-- import PlutusTx (UnsafeFromData(unsafeFromBuiltinData))
+-- 
+-- -- Place for future imports
+-- import PlutusLedgerApi.V1 (PubKeyHash(..),Credential(..),ScriptHash(..))
 --
 --
 --
@@ -29,38 +29,38 @@ import PlutusLedgerApi.V1 (PubKeyHash(..),Credential(..),ScriptHash(..))
 --
 --
 
-assocMap :: AssocMap.Map k v
-assocMap = AssocMap.unsafeFromList mempty
-
-unsafeFromBuiltinData :: Integer
-unsafeFromBuiltinData =
-  Tx.unsafeFromBuiltinData (error "we don't care")
-
-usageOfPTxMaybe :: Integer
-usageOfPTxMaybe = let
-  x = Maybe.fromMaybe 0 (Maybe.Just 1)
-  in x
-
-pubKeyHashEq :: Bool
-pubKeyHashEq = pubKeyHash == pubKeyHash
-  where
-    pubKeyHash :: PubKeyHash
-    pubKeyHash = error "we don't care"
-
-scriptHashEq :: Bool
-scriptHashEq = scriptHash == scriptHash
-  where
-    scriptHash :: ScriptHash
-    scriptHash = error "we don't care"
-
-credentialHashEq :: Bool
-credentialHashEq = credentialHash == credentialHash
-  where
-    credentialHash :: Credential
-    credentialHash = error "we don't care"
-
-credentialHashLe :: Bool
-credentialHashLe = credentialHash < credentialHash
-  where
-    credentialHash :: Credential
-    credentialHash = error "we don't care"
+-- assocMap :: AssocMap.Map k v
+-- assocMap = AssocMap.unsafeFromList mempty
+-- 
+-- unsafeFromBuiltinData :: Integer
+-- unsafeFromBuiltinData =
+--   Tx.unsafeFromBuiltinData (error "we don't care")
+-- 
+-- usageOfPTxMaybe :: Integer
+-- usageOfPTxMaybe = let
+--   x = Maybe.fromMaybe 0 (Maybe.Just 1)
+--   in x
+-- 
+-- pubKeyHashEq :: Bool
+-- pubKeyHashEq = pubKeyHash == pubKeyHash
+--   where
+--     pubKeyHash :: PubKeyHash
+--     pubKeyHash = error "we don't care"
+-- 
+-- scriptHashEq :: Bool
+-- scriptHashEq = scriptHash == scriptHash
+--   where
+--     scriptHash :: ScriptHash
+--     scriptHash = error "we don't care"
+-- 
+-- credentialHashEq :: Bool
+-- credentialHashEq = credentialHash == credentialHash
+--   where
+--     credentialHash :: Credential
+--     credentialHash = error "we don't care"
+-- 
+-- credentialHashLe :: Bool
+-- credentialHashLe = credentialHash < credentialHash
+--   where
+--     credentialHash :: Credential
+--     credentialHash = error "we don't care"
